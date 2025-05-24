46c71e3578b89d4b821e4b10b957ac72bf880220
ARJUN CHANDRA DAS <arjundasback515@gmail.com>
2025-05-25 00:15:51 +0600

    create: sightings table insert data and show all data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -61,3 +61,51 @@ VALUES (
         '1758-01-01',
         'Endangered'
     );
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INTEGER REFERENCES species (species_id),
+    ranger_id INTEGER REFERENCES rangers (ranger_id),
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
+
+SELECT * FROM sightings;
+
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
