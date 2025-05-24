1bfbabcc113d2f1b661a82c66c56afdb20969ac6
ARJUN CHANDRA DAS <arjundasback515@gmail.com>
2025-05-25 00:10:36 +0600

    create: species table insert table data and show data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -19,3 +19,45 @@ VALUES (
         'Carol King',
         'Mountain Range'
     );
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE,
+    conservation_status VARCHAR(50)
+);
+
+SELECT * FROM species;
+
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
